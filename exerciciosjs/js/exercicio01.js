92e3522fe1f05b237d4693a0fd2fe7879d1abbf2
Roberson Alves <robersonjfa@gmail.com>
2024-03-08 13:37:54 -0300

    Adicionando arquivos

--- exerciciosjs/js/exercicio01.js
+++ exerciciosjs/js/exercicio01.js
@@ -1,9 +1,11 @@
 function verificar() {
-  var numero = parseInt(
+  var numero = Number(
     document.getElementById("numero").value);
+  var conteudo = document.getElementById("conteudo");
 
   if (numero < 10) {
     alert("Número menor que 10!");
+    conteudo.innerHTML = "Número menor que 10!";
   }
 }
 
